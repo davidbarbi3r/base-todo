0512cbf60922aeb97d75c9fbf5b2d456589d9549
davidbarbi3r <davidbarbi3r@gmail.com>
2022-09-28 22:56:18 +0200

    :sparkles: Add route authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
-import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Login from "./pages/Login"
-import TodoApp from "./pages/TodoApp"
-import './App.css'
-import SignUp from './pages/SignUp'
-import { Box, Container } from '@mui/material'
-import { onAuthStateChanged, User } from "firebase/auth"
-import { auth } from './config/config'
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import TodoApp from "./pages/TodoApp";
+import SignUp from "./pages/SignUp";
+import { Box, Container } from "@mui/material";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "./config/config";
+import ITodo from "./interface/todo";
+import AuthRoute from "./modules/AuthRoute";
 
 function App() {
-  const [user, setUser] = useState<User | null>(null)
-
+  const [user, setUser] = useState<User | null>(null);
+  
   onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser)
-  })
+    setUser(currentUser);
+  });
 
   return (
-    <Container >
+    <Container>
       <Routes>
-        <Route path="/" element={<TodoApp user={user}/>}/>
-        <Route path="/login" element={<Login user={user}/>}/>
-        <Route path="/signup" element={<SignUp user={user}/>}/>
+        <Route
+          path="/"
+          element={
+            <AuthRoute>
+              <TodoApp user={user}/>
+            </AuthRoute>
+          }
+        />
+        <Route path="/login" element={<Login user={user} />} />
+        <Route path="/signup" element={<SignUp user={user} />} />
       </Routes>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/modules/AuthRoute.tsx
@@ -0,0 +1,29 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import logging from "../config/logging";
+
+export interface IAuthRouteProps {
+  children: any;
+}
+
+const AuthRoute = (props: IAuthRouteProps) => {
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const AuthCheck = onAuthStateChanged(auth, (user) => {
+      if (user) {
+      } else {
+        logging.warn("Unauthorized, you have to login");
+        navigate("/login");
+      }
+    });
+    return () => AuthCheck();
+  }, [auth]);
+
+  return <>{props.children}</>
+
+};
+
+export default AuthRoute;
