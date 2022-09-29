5be100def360e596a4a471f0b8aa2d2b17f0dbf2
davidbarbi3r <davidbarbi3r@gmail.com>
2022-09-29 16:57:50 +0200

    :lipstick: refactoring & styling todo card

---
+++ src/components/Todo.tsx
@@ -0,0 +1,102 @@
+import {
+  FormControlLabel,
+  Grid,
+  Button,
+  ButtonGroup,
+  TextField,
+  ListItem,
+  Divider,
+  ListItemText,
+  Chip,
+  Checkbox,
+} from "@mui/material";
+import ITodo from "../interface/todo";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SaveAltOutlinedIcon from "@mui/icons-material/SaveAltOutlined";
+import PriorityHighIcon from "@mui/icons-material/PriorityHigh";
+import { useState } from "react";
+import { doc, deleteDoc } from "firebase/firestore";
+import { database } from "../config/config";
+import { crud } from "../controllers/CRUDcontrollers";
+
+type Props = {
+  todo: ITodo;
+};
+
+export default function DisplayTodo({ todo }: Props) {
+  const [edit, setEdit] = useState(false);
+  const [editedTodo, setEditedTodo] = useState<ITodo>(todo);
+
+  return (
+    <>
+      <ListItem sx={todo.done ? {backgroundColor: "lightgray", borderRadius: "5px", mb: 0.5} : {}}>
+        <Grid
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          width={"100%"}
+        >
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={todo.done}
+                onChange={(e) => crud.update(editedTodo, setEdit, todo.id, e)}
+              />
+            }
+            label=""
+          />
+          {edit ? (
+            <TextField
+              autoFocus
+              defaultValue={todo.text}
+              onChange={(e) => setEditedTodo({ ...todo, text: e.target.value })}
+            ></TextField>
+          ) : (
+            <>
+              <ListItemText
+                sx={todo.done? {textDecoration: "line-through"} : {}}
+                primary={todo.text}
+                secondary={
+                  todo.echeanceDate ? todo.echeanceDate?.toString() : ""
+                }
+              ></ListItemText>
+              {todo.important && (
+                <Chip
+                  sx={{ marginRight: "2em" }}
+                  icon={<PriorityHighIcon />}
+                  variant="outlined"
+                  color="warning"
+                  size="small"
+                  label="important"
+                />
+              )}
+            </>
+          )}
+          <Grid display={"flex"} alignItems={"right"}></Grid>
+
+          <ButtonGroup
+            size="small"
+            variant="outlined"
+            aria-label="outlined button group"
+          >
+            {edit ? (
+              <Button onClick={() => crud.update(editedTodo, setEdit, todo.id)}>
+                <SaveAltOutlinedIcon />
+              </Button>
+            ) : (
+              ""
+            )}
+            <Button onClick={() => setEdit((prev) => !prev)}>
+              <EditIcon />
+            </Button>
+            <Button onClick={() => crud.delete(todo.id)}>
+              <DeleteIcon />
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </ListItem>
+      <Divider />
+    </>
+  );
+}
