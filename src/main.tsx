6db8575ab6a8613f904f34cd22b87621055f6ea8
davidbarbi3r <davidbarbi3r@gmail.com>
2022-09-30 12:00:28 +0200

    :bug: Using hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { CssBaseline } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CssBaseline/>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
